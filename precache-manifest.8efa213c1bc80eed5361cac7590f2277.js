fcdd9fa40980c051dd4fa61517de8c214425874e
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2020-09-10 14:35:01 +0300

    Updates

---
+++ precache-manifest.8efa213c1bc80eed5361cac7590f2277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4404997765c429547e2b0f6416c7168",
+    "url": "/pizza-app/index.html"
+  },
+  {
+    "revision": "291d43d891a98b6c074c",
+    "url": "/pizza-app/static/css/main.5eaf7b68.chunk.css"
+  },
+  {
+    "revision": "e727dcb5365a4b81f14a",
+    "url": "/pizza-app/static/js/2.48e52bb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza-app/static/js/2.48e52bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291d43d891a98b6c074c",
+    "url": "/pizza-app/static/js/main.c096bbcf.chunk.js"
+  },
+  {
+    "revision": "8391fda3eadf6f9f7d08",
+    "url": "/pizza-app/static/js/runtime-main.2a1d5b94.js"
+  }
+]);
